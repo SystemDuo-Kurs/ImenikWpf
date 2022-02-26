4f6534df7adee59c9b675b5f474a38fe7c420a75
Dragan Pepic <d.pepic@hotmail.com>
2022-02-26 18:04:57 +0100

    sdgsdg

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,18 +27,75 @@ namespace ImenikWpf
         public MainWindow()
         {
             InitializeComponent();
+
             //while (true)
             // {
             //     Osobe.Add(new Osoba { Ime = "asdasd", Prezime = "asdasd" });
             //}
+
+            if (File.Exists("osobe.txt"))
+            {
+                foreach (string red in File.ReadLines("osobe.txt"))
+                {
+                    string[] polja = red.Split(";");
+                    Osobe.Add(new Osoba
+                    {
+                        Ime = polja[0],
+                        Prezime = polja[1],
+                        BrojTelefona = polja[2],
+                        Email = polja[3]
+                    });
+                }
+            }
             DataContext = new Osoba();
             dgOsoba.ItemsSource = Osobe;
         }
 
         private void Dodaj(object sender, RoutedEventArgs e)
         {
-            Osobe.Add(DataContext as Osoba);
+            if (!(DataContext is Osoba o && Osobe.Contains(o)))
+            {
+                Osobe.Add(DataContext as Osoba);
+            }
             DataContext = new Osoba();
+            dgOsoba.SelectedItem = null;
+        }
+
+        private void Brisanje(object sender, RoutedEventArgs e)
+        {
+            if (!Osobe.Remove(dgOsoba.SelectedItem as Osoba))
+            {
+                MessageBox.Show("Selektuj nesto brt");
+            }
+        }
+
+        private void Izmena(object sender, RoutedEventArgs e)
+        {
+            DataContext = dgOsoba.SelectedItem;
+        }
+
+        private void Promena(object sender, SelectionChangedEventArgs e)
+        {
+            if (dgOsoba.SelectedItem is null)
+            {
+                dugmeBrisanje.IsEnabled = false;
+                dugmeIzmena.IsEnabled = false;
+            }
+            else
+            {
+                dugmeBrisanje.IsEnabled = true;
+                dugmeIzmena.IsEnabled = true;
+            }
+        }
+
+        private void Zatvaranje(object sender, EventArgs e)
+        {
+            if (File.Exists("osobe.txt"))
+            {
+                File.Delete("osobe.txt");
+            }
+            Osobe.ToList().ForEach(o => File.AppendAllText("osobe.txt",
+                $"{o.Ime};{o.Prezime};{o.BrojTelefona};{o.Email}{Environment.NewLine}"));
         }
     }
 }
--- Osoba.cs
+++ Osoba.cs
@@ -1,16 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ImenikWpf
 {
-    public class Osoba
+    public class Osoba : INotifyPropertyChanged
     {
-        public string Ime { get; set; } = string.Empty;
-        public string Prezime { get; set; } = string.Empty;
-        public string BrojTelefona { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
+        private string _ime = string.Empty;
+
+        public string Ime
+        {
+            get
+            {
+                return _ime;
+            }
+            set
+            {
+                _ime = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Ime"));
+            }
+        }
+
+        private string _prezime = string.Empty;
+
+        public string Prezime
+        {
+            get
+            {
+                return _prezime;
+            }
+            set
+            {
+                _prezime = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Prezime"));
+            }
+        }
+
+        private string _brojTelefona = string.Empty;
+
+        public string BrojTelefona
+        {
+            get
+            {
+                return _brojTelefona;
+            }
+            set
+            {
+                _brojTelefona = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BrojTelefona"));
+            }
+        }
+
+        private string _email = string.Empty;
+
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Email"));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Osoba o && o.BrojTelefona == BrojTelefona && o.Email == Email)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return BrojTelefona.GetHashCode() + Email.GetHashCode();
+        }
     }
 }
